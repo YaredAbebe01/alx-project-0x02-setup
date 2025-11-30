46939125497102c0154c5c68043be90a6fbce8e0
Yared Abebe <yaredabebe0101@gmail.com>
2025-11-30 13:16:20 +0300

    Add Next.js project properly

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="w-full p-4 bg-blue-600 text-white text-center text-xl font-bold">
+      Next.js Project Setup – Header
+    </header>
+  );
+};
+
+export default Header;
