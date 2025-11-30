9a2b1f41caba648d91235e2743afc47b2f734a69
Yared Abebe <yaredabebe0101@gmail.com>
2025-11-30 13:18:40 +0300

    Add Next.js project properly

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/layout/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main className="p-6 text-center text-2xl">
+        Welcome to the Next.js ALX Project Setup!
+      </main>
+    </div>
+  );
+}
